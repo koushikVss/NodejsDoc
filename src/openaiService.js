b9f79598c044ef1cf2ca5468b06db685051ef81a
jmnayea <koushik.ssv@gmail.com>
2024-12-07 20:26:44 +0530

    key change

--- src/openaiService.js
+++ src/openaiService.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 import config from '../config.json'; // Adjust the path as necessary
 
-const API_KEY = config.REACT_APP_OPENAI_API_KEY;
+const apicode = config.REACT_APP_OPENAI_APICODE;
 
 const openai = axios.create({
     baseURL: 'https://api.openai.com/v1',
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${API_KEY}`,
+        'Authorization': `Bearer ${apicode}`,
     },
 });
 
 export const getOpenAIResponse = async (messages) => {
-    console.log("key ", API_KEY); // This will log the API key
+    // console.log("key ", apicode); 
     const response = await openai.post('/chat/completions', {
         model: 'gpt-3.5-turbo',
         messages: messages,
