4125e18b987d953f90d1ea3d7067b0d4f50c9fb9
jmnayea <koushik.ssv@gmail.com>
2024-12-07 20:32:29 +0530

    ke

--- src/openaiService.js
+++ src/openaiService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import config from '../config.json'; // Adjust the path as necessary
+import config from './config.json'; // Adjust the path as necessary
 
 const apicode = config.REACT_APP_OPENAI_APICODE;
 
